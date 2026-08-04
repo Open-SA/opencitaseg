65012bb264238adeccf17819c648753a17248cd9
Joaquin <jetchegaray@opensa.com.ar>
2026-08-04 12:37:21 -0300

    chore: add .gitignore, untrack vendor/

---
+++ public/js/locales/en_GB.js
@@ -0,0 +1,11 @@
+/*
+ * opencitaseg - archivo generado automaticamente por tools/build-js-locales.py
+ * NO editar a mano: los cambios se pierden en la proxima generacion.
+ * Fuente: locales/en_GB.po
+ */
+window.OPENCITASEG_I18N = {
+  "Quote": "Quote",
+  "Quote this followup": "Quote this followup",
+  "Quoting %s": "Quoting %s",
+  "User": "User"
+};
---
+++ public/js/locales/fr_FR.js
@@ -0,0 +1,11 @@
+/*
+ * opencitaseg - archivo generado automaticamente por tools/build-js-locales.py
+ * NO editar a mano: los cambios se pierden en la proxima generacion.
+ * Fuente: locales/fr_FR.po
+ */
+window.OPENCITASEG_I18N = {
+  "Quote": "Citer",
+  "Quote this followup": "Citer ce suivi",
+  "Quoting %s": "Citation de %s",
+  "User": "Utilisateur"
+};
