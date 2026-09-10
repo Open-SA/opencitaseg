9ddde6a841f5e32057f52f9d1e5d2a93f3eb87a6
Joaquin <jetchegaray@opensa.com.ar>
2026-09-10 15:43:23 -0300

    dejar los define en los valores de GLPI 11 mientras hago cherry pick

--- setup.php
+++ setup.php
@@ -32,13 +32,13 @@ use GlpiPlugin\Opencitaseg\CiteNotification;
 
 // El bump a 1.2.0 va en su propia rama chore/release-1.2.0 (guia §7),
 // no en la rama feat/notificaciones-cita.
-define('PLUGIN_OPENCITASEG_VERSION', '1.1.3');
+define('PLUGIN_OPENCITASEG_VERSION', '1.2.0');
 
 // Minimal GLPI version, inclusive
-define("PLUGIN_OPENCITASEG_MIN_GLPI_VERSION", "10.0.0");
+define("PLUGIN_OPENCITASEG_MIN_GLPI_VERSION", "11.0.0");
 
 // Maximum GLPI version, exclusive
-define("PLUGIN_OPENCITASEG_MAX_GLPI_VERSION", "10.0.99");
+define("PLUGIN_OPENCITASEG_MAX_GLPI_VERSION", "11.0.99");
 
 /**
  * Init hooks of the plugin.
