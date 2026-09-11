420b4e071805396701b1ee1e5b3f20e380cee614
Joaquin <jetchegaray@opensa.com.ar>
2026-09-11 15:02:50 -0300

    corrijo instalacion notif Closes #11

--- hook.php
+++ hook.php
@@ -94,6 +94,10 @@ function plugin_opencitaseg_install()
         $DB->error()
     );
 
+    if (! CiteNotification::install()) {
+        return false;
+    }
+
     return true;
 }
 
@@ -106,6 +110,8 @@ function plugin_opencitaseg_uninstall()
             $DB->doQueryOrDie("DROP TABLE `$table`", $DB->error());
         }
     }
+
+    CiteNotification::uninstall();
 
     return true;
 }
