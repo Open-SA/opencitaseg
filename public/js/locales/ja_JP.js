ef617980633518cdb7301e427e7eec0a0ee26fec
INOUE Daisuke <inoue.daisuke@gmail.com>
2026-09-22 17:44:55 +0900

    Japanese translation for released 11.2.1 .

--- public/js/locales/ja_JP.js
+++ public/js/locales/ja_JP.js
@@ -4,10 +4,10 @@
  * Fuente: locales/ja_JP.po
  */
 window.OPENCITASEG_I18N = {
-  "Date of the quote": "Fecha de la cita",
+  "Date of the quote": "引用日",
   "Quote": "引用",
   "Quote this followup": "この回答を引用",
   "Quoting %s": "%s の回答を引用",
-  "Unknown user": "Usuario desconocido",
+  "Unknown user": "不明なユーザー",
   "User": "ユーザー"
 };
